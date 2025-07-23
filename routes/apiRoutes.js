c988e4ee459f33f03f5737057708d2333d879015
balu-bunny <balakrishna@qualityze.com>
2025-07-23 09:45:27 +0530

    added env.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,5 +6,6 @@ const backupController = require('../controllers/backupController');
 router.get('/orgs', orgController.getOrgs);
 router.post('/backup', backupController.backupData);
 router.post('/addNewOrg', orgController.addNewOrg);
+router.post('/envvariable', orgController.addNewOrg);
 
 module.exports = router;
